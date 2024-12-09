a75a432977f5871a74d8c8a0d840b1f4cb7326b2
angel <legosalo@yandex.ru>
2024-12-09 18:31:08 +0300

    first commit

---
+++ demo/src/main/java/com/diary/demo/EmotionDiaryApplication.java
@@ -0,0 +1,15 @@
+package com.diary.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmotionDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmotionDiaryApplication.class, args);
+	}
+
+
+
+}
---
+++ demo/src/main/java/com/diary/demo/Repository/MyUserRepository.java
@@ -0,0 +1,10 @@
+package com.diary.demo.Repository;
+
+import com.diary.demo.Models.MyUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface MyUserRepository extends JpaRepository<MyUser, Long> {
+
+    Optional<MyUser> findByUsername(String username);
+}
